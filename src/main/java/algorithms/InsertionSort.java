7056c5794d6ed83f0309ea95b2e435e86c312f5e
gaziza <241375@astanait.edu.kz>
2025-10-05 11:22:36 +0500

    feat(metrics): add PerformanceTracker and integrate with InsertionSort

--- src/main/java/algorithms/InsertionSort.java
+++ src/main/java/algorithms/InsertionSort.java
@@ -1,23 +1,32 @@
 package algorithms;
 
+import metrics.PerformanceTracker;
+
 public class InsertionSort {
 
-
-    public static void sort(int[] arr) {
+    public static void sort(int[] arr, PerformanceTracker tracker) {
         int n = arr.length;
         for (int i = 1; i < n; i++) {
             int key = arr[i];
+            tracker.incrementArrayAccesses();
             int j = i - 1;
 
-
-            while (j >= 0 && arr[j] > key) {
-                arr[j + 1] = arr[j];
-                j--;
+            while (j >= 0) {
+                tracker.incrementComparisons();
+                tracker.incrementArrayAccesses();
+                if (arr[j] > key) {
+                    arr[j + 1] = arr[j];
+                    tracker.incrementArrayAccesses();
+                    tracker.incrementSwaps();
+                    j--;
+                } else {
+                    break;
+                }
             }
             arr[j + 1] = key;
+            tracker.incrementArrayAccesses();
         }
     }
-
 
     public static void printArray(int[] arr) {
         for (int num : arr) {
@@ -26,16 +35,20 @@ public class InsertionSort {
         System.out.println();
     }
 
-
     public static void main(String[] args) {
         int[] arr = {5, 3, 4, 1, 2};
+
+        PerformanceTracker tracker = new PerformanceTracker();
+
         System.out.println("Before sorting:");
         printArray(arr);
 
-        sort(arr);
+        sort(arr, tracker);
 
         System.out.println("After sorting:");
         printArray(arr);
+
+        System.out.println("\nPerformance metrics:");
+        tracker.printMetrics();
     }
 }
-
---
+++ src/main/java/metrics/PerformanceTracker.java
@@ -0,0 +1,44 @@
+package metrics;
+
+public class PerformanceTracker {
+
+    private long comparisons;
+    private long swaps;
+    private long arrayAccesses;
+
+    public void reset() {
+        comparisons = 0;
+        swaps = 0;
+        arrayAccesses = 0;
+    }
+
+    public void incrementComparisons() {
+        comparisons++;
+    }
+
+    public void incrementSwaps() {
+        swaps++;
+    }
+
+    public void incrementArrayAccesses() {
+        arrayAccesses++;
+    }
+
+    public long getComparisons() {
+        return comparisons;
+    }
+
+    public long getSwaps() {
+        return swaps;
+    }
+
+    public long getArrayAccesses() {
+        return arrayAccesses;
+    }
+
+    public void printMetrics() {
+        System.out.println("Comparisons: " + comparisons);
+        System.out.println("Swaps: " + swaps);
+        System.out.println("Array accesses: " + arrayAccesses);
+    }
+}
